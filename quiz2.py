cc08e069ea6770c08f07340dfddc849cd981a274
Julian Higuera <julianhjessen@hotmail.com>
2019-06-17 18:54:57 -0400

    gr8 job fag

---
+++ quiz2.py
@@ -0,0 +1,17 @@
+question2 = input("Who is the hottest man alive? ")
+a = "Keanu Reeves"
+b = "Robert E. O. Speedwagon"
+c = "Optimus Prime"
+d = "Anakin Skywalker"
+
+print("a) Keanu Reeves")
+print("b) Robert E. O. Speedwagon")
+print("c) Optimus Prime")
+print("d) Anakin Skywalker")
+
+answer2 = input(question2)
+
+if answer2 == "a":
+    print("correct")
+else:
+    print("incorrect")
